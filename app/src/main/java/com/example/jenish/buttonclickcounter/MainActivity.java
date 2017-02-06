7d79a061be3ce035899788f4ba21f9e193ca4b0f
Jenish Patel <jenishpat@gmail.com>
2017-02-05 23:05:25 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/jenish/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/example/jenish/buttonclickcounter/MainActivity.java
@@ -95,6 +95,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy: Destroy");
+        Log.d(TAG, "onDestroy: Destroy"); //Delete this please
     }
 }
